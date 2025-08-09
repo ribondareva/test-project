47451f6e08275ae70f7231092691963386399708
ribondareva <msbondareva01@gmail.com>
2025-08-09 22:07:18 +0300

    fix(urls): add app_name to vehicle urls for namespace compatibility

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("vehicle.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('vehicle/', include('vehicle.urls', namespace='vehicle')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
